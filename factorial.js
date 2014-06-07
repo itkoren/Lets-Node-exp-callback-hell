0864b0ccf6d505726550d9d43242f024a58eaa2f
Itai Koren <itkoren@gmail.com>
2014-06-08 00:35:40 +0300

    remove unneeded settimeout
    Signed-off-by: Itai Koren <itkoren@gmail.com>

--- factorial.js
+++ factorial.js
@@ -4,19 +4,11 @@ var factorial = function(n, callback) {
     var fact = 1;
     var index = 1;
 
-    setTimeout(function(){
-        console.log("TIMEOUT 0");
-    }, 0);
-
     async.whilst(
         function() {
             return index <= n;
         },
         function(next) {
-            setTimeout(function(){
-                console.log("TIMEOUT 0");
-            }, 0);
-
             setImmediate(function() {
                 console.log("step" + index);
                 fact = fact * index;
@@ -26,16 +18,16 @@ var factorial = function(n, callback) {
         },
         function(err) {
             setImmediate(function() {
-                callback(null, fact);
+                callback(err, fact);
             });
         }
     );
 };
 
-factorial(5, function(err, result) {
-    console.log("COMPLETED: " + result);
-});
-
 setTimeout(function(){
     console.log("TIMEOUT 0");
 }, 0);
+
+factorial(50, function(err, result) {
+    console.log("COMPLETED: " + result);
+});
